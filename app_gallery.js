c18cdfa1116ea3574b5157a80db5493a99283bfc
Kacper Burza <65431499+kburza@users.noreply.github.com>
2022-07-25 20:04:35 -0400

    Add files via upload

--- app_gallery.js
+++ app_gallery.js
@@ -187,3 +187,18 @@ sliders.forEach(slider => {
   appearOnScroll.observe(slider);
 });
 */
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container', {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
